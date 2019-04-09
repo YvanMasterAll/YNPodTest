085955226da8d655c81805a4df653374af48334f
Yiqiang Zeng <1136838669@qq.com>
2019-04-09 19:28:06 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-YNPodTest_Tests/Pods-YNPodTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YNPodTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YNPodTest_Tests
+@end
---
+++ Example/Pods/Target Support Files/YNPodTest/YNPodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YNPodTest : NSObject
+@end
+@implementation PodsDummy_YNPodTest
+@end
